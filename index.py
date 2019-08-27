d72ee511db2b29e2a3f261d0011ae33067377124
autumn-ragland <raglanda998@gmail.com>
2019-08-27 14:16:46 -0500

    Graded 08-27-2019 14:14:52

--- index.py
+++ index.py
@@ -1,22 +1,16 @@
 def main():
-    # pro1()
-    # pro2()
-    # pro3()
-    # pro4()
+    pro1()
+    pro2()
+    pro3()
+    pro4()
     pro5()
 # Problem 1:
-#
 # Create a function with the variable below. After you create the variable do the instructions below that.
-#
 # arrayForProblem2 = ["Kenn", "Kevin", "Erin", "Meka"]
 # a) Print the 3rd element of the numberList.
-#
-#     b) Print the size of the array
-#
+# b) Print the size of the array
 # c) Delete the second element.
-#
-#     d) Print the 3rd element.
-#
+# d) Print the 3rd element.
 def pro1():
     def ghost():
         arrayFor = ["Kenn", "Kevin", "Erin", "Meka"]
@@ -26,28 +20,14 @@ def pro1():
         print(arrayFor[2])
     ghost()
 
-#     Problem 2:
-#
+# Problem 2:
 # We will keep having this problem until EVERYONE gets it right without help
-#
 # Create a function that has a loop that quits with ‘q’. If the user doesn't enter 'q', ask them to input another string.
-#
 def pro2():
     user=""
     while user !="q":
         user = input("Enter somthing\n")
-# Problem 3:
-#
-#     Create a function that contains a collection of information for the following. After you create the collection do the instructions below that.
-#
-# Jonathan/John
-# Michael/Mike
-# William/Bill
-# Robert/Rob
-# a) Print the collection
-#
-# b) Print William's nickname
-#
+
 def pro3():
     def collect():
         name = ["Jonathan","Michael", "William","Robert"]
@@ -58,18 +38,15 @@ def pro3():
         print(nnames[2])
     collect()
 # Problem 4:
-#
-#     Create an array of 5 numbers. Using a loop, print the elements in the array reverse order. Do not use a function
-#
+# Create an array of 5 numbers. Using a loop, print the elements in the array reverse order. Do not use a function
 def pro4():
     array = [1,2,3,4,5]
     f = range(int(len(array))-1,-1,-1)
     for x in f:
         print(array[x])
-# Problem 5:
-#
-# Create a function that will have a hard coded array then ask the user for a number. Use the userInput to state how many numbers in an array are higher, lower, or equal to it.
 
+# Problem 5:
+# Create a function that will have a hard coded array then ask the user for a number. Use the userInput to state how many numbers in an array are higher, lower, or equal to it.
 def pro5():
     def fin():
         high = 0
